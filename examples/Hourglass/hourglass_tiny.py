4938b368beeed96b4bd68bb1339d8261b6b65d60
Tao Hu <taohu620@gmail.com>
2018-01-18 22:31:33 +0800

    rearrange

--- examples/Hourglass/hourglass_tiny.py
+++ examples/Hourglass/hourglass_tiny.py
@@ -7,50 +7,33 @@ import os
 from tensorpack.utils import logger
 from config import process_config
 
+config = process_config()
+
 class HourglassModel():
-	def __init__(self, nFeat = 512, nStack = 4, nModules = 1, nLow = 4, outputDim = 16, batch_size = 16, drop_rate = 0.2, lear_rate = 2.5e-4, decay = 0.96, decay_step = 2000, dataset = None, training = True, w_summary = True, logdir_train = None, logdir_test = None,tiny = True, attention = False,modif = True,w_loss = False, name = 'tiny_hourglass',  joints = ['r_anckle', 'r_knee', 'r_hip', 'l_hip', 'l_knee', 'l_anckle', 'pelvis', 'thorax', 'neck', 'head', 'r_wrist', 'r_elbow', 'r_shoulder', 'l_shoulder', 'l_elbow', 'l_wrist']):
-		""" Initializer
-		Args:
-			nStack				: number of stacks (stage/Hourglass modules)
-			nFeat				: number of feature channels on conv layers
-			nLow				: number of downsampling (pooling) per module
-			outputDim			: number of output Dimension (16 for MPII)
-			batch_size			: size of training/testing Batch
-			dro_rate			: Rate of neurons disabling for Dropout Layers
-			lear_rate			: Learning Rate starting value
-			decay				: Learning Rate Exponential Decay (decay in ]0,1], 1 for constant learning rate)
-			decay_step			: Step to apply decay
-			dataset			: Dataset (class DataGenerator)
-			training			: (bool) True for training / False for prediction
-			w_summary			: (bool) True/False for summary of weight (to visualize in Tensorboard)
-			tiny				: (bool) Activate Tiny Hourglass
-			attention			: (bool) Activate Multi Context Attention Mechanism (MCAM)
-			modif				: (bool) Boolean to test some network modification # DO NOT USE IT ! USED TO TEST THE NETWORK
-			name				: name of the model
-		"""
-		self.nStack = nStack
-		self.nFeat = nFeat
-		self.nModules = nModules
-		self.outDim = outputDim
-		self.batchSize = batch_size
+	def __init__(self, dataset, training = True, w_summary = True, modif = True):
+		self.nStack = config['nstacks']
+		self.nFeat = config['nfeats']
+		self.nModules = config['nmodules']
+		self.outDim = config['num_joints']
+		self.batchSize = config['batch_size']
 		self.training = training
 		self.w_summary = w_summary
-		self.tiny = tiny
-		self.dropout_rate = drop_rate
-		self.learning_rate = lear_rate
-		self.decay = decay
-		self.name = name
-		self.attention = attention
-		self.decay_step = decay_step
-		self.nLow = nLow
+		self.tiny = config['tiny']
+		self.dropout_rate = config['dropout_rate']
+		self.learning_rate = config['learning_rate']
+		self.decay = config['learning_rate_decay']
+		self.name = config['name']
+		self.attention = config['mcam']
+		self.decay_step = config['decay_step']
+		self.nLow = config['nlow']
 		self.modif = modif
 		self.dataset = dataset
 		self.cpu = '/cpu:0'
 		self.gpu = '/gpu:0'
-		self.logdir_train = logdir_train
-		self.logdir_test = logdir_test
-		self.joints = joints
-		self.w_loss = w_loss
+		self.logdir_train = config['log_dir_train']
+		self.logdir_test = config['log_dir_test']
+		self.joints = config['joint_list']
+		self.w_loss = config['weighted_loss']
 		
 	# ACCESSOR
 	
---
+++ examples/Hourglass/train_launcher.py
@@ -0,0 +1,27 @@
+"""
+TRAIN LAUNCHER 
+
+"""
+
+import os
+from hourglass_tiny import HourglassModel
+from datagen import DataGenerator
+from tensorpack.utils import logger
+from config import process_config
+
+logger.auto_set_dir()
+os.environ['CUDA_VISIBLE_DEVICES'] ='2'
+
+
+if __name__ == '__main__':
+	config = process_config()
+	dataset = DataGenerator(config['joint_list'], config['img_directory'], config['training_txt_file'], remove_joints=config['remove_joints'])
+	dataset._create_train_table()
+	dataset._randomize()
+	dataset._create_sets()
+	
+	model = HourglassModel( training=True,modif=False,dataset=dataset)
+
+	model.generate_model()
+	model.training_init(nEpochs=config['nepochs'], epochSize=config['epoch_size'], saveStep=config['saver_step'], dataset = None)
+	
