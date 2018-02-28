18dfac2783937e4f434a65cfd8ccbb646cccd275
Tao Hu <taohu620@gmail.com>
2018-02-28 17:38:19 +0800

    u

--- examples/Deeplab/experiment2/deeplabv2.cs.imagesize673.scale18.py
+++ examples/Deeplab/experiment2/deeplabv2.cs.imagesize673.scale18.py
@@ -28,48 +28,25 @@ from resnet_model import (
     resnet_backbone_deeplab)
 
 
-CLASS_NUM = 21
-CROP_SIZE = 473
+CLASS_NUM = 19
+IMAGE_H = 673
+IMAGE_W = 673
 IGNORE_LABEL = 255
 
 first_batch_lr = 2.5e-4
 lr_schedule = [(2, 1e-4), (4, 1e-5), (6, 8e-6)]
-epoch_scale = 8
+epoch_scale = 18
 max_epoch = 10
 lr_multi_schedule = [('aspp.*_conv/W', 5),('aspp.*_conv/b',10)]
-batch_size = 15
+batch_size = 5
 evaluate_every_n_epoch = max_epoch
-
-def get_data(name, data_dir, meta_dir, batch_size):
-    isTrain = True if 'train' in name else False
-    ds = dataset.PascalVOC12(data_dir, meta_dir, name, shuffle=True)
-
-    if isTrain:#special augmentation
-        shape_aug = [imgaug.RandomResize(xrange=(0.7, 1.5), yrange=(0.7, 1.5),
-                            aspect_ratio_thres=0.15),
-                     RandomCropWithPadding(CROP_SIZE,IGNORE_LABEL),
-                     imgaug.Flip(horiz=True),
-                     ]
-    else:
-        shape_aug = []
-
-    ds = AugmentImageComponents(ds, shape_aug, (0, 1), copy=False)
-
-
-    #ds = FakeData([[CROP_SIZE, CROP_SIZE, 3], [CROP_SIZE, CROP_SIZE]], 5000, random=False, dtype='uint8')
-    if isTrain:
-        ds = BatchData(ds, batch_size)
-        ds = PrefetchDataZMQ(ds, 1)
-    else:
-        ds = BatchData(ds, 1)
-    return ds
 
 class Model(ModelDesc):
 
     def _get_inputs(self):
         ## Set static shape so that tensorflow knows shape at compile time.
-        return [InputDesc(tf.float32, [None, CROP_SIZE, CROP_SIZE, 3], 'image'),
-                InputDesc(tf.int32, [None, CROP_SIZE, CROP_SIZE], 'gt')]
+        return [InputDesc(tf.float32, [None, IMAGE_H, IMAGE_W, 3], 'image'),
+                InputDesc(tf.int32, [None, IMAGE_H, IMAGE_W], 'gt')]
 
     def _build_graph(self, inputs):
         def resnet101(image):
@@ -92,7 +69,7 @@ class Model(ModelDesc):
                 with argscope([Conv2D, MaxPooling, GlobalAvgPooling, BatchNorm], data_format="NHWC"):
                     return resnet_backbone_deeplab(
                         image, num_blocks,
-                        preresnet_group if mode == 'preact' else resnet_group_deeplab, block_func,CLASS_NUM,ASPP = False)
+                        preresnet_group if mode == 'preact' else resnet_group_deeplab, block_func, class_num = CLASS_NUM,ASPP = False)
 
             return get_logits(image)
 
@@ -100,6 +77,7 @@ class Model(ModelDesc):
         image = image - tf.constant([104, 116, 122], dtype='float32')
         label = tf.identity(label, name="label")
 
+        #predict = vgg16(image)
         predict = resnet101(image)
 
         costs = []
@@ -130,13 +108,39 @@ class Model(ModelDesc):
         opt = tf.train.AdamOptimizer(lr, epsilon=2.5e-4)
         return optimizer.apply_grad_processors(
             opt, [gradproc.ScaleGradient(
-                lr_multi_schedule)])
+                [('aspp.*_conv/W', 10),('aspp.*_conv/b',20)])])
 
 
+def get_data(name, meta_dir, batch_size):
+    isTrain = name == 'train'
+    ds = dataset.Cityscapes(meta_dir, name, shuffle=True)
 
 
-def view_data(data_dir, meta_dir, batch_size):
-    ds = RepeatedData(get_data('train_aug',data_dir, meta_dir, batch_size), -1)
+    if isTrain:#special augmentation
+        shape_aug = [imgaug.RandomResize(xrange=(0.7, 1.5), yrange=(0.7, 1.5),
+                            aspect_ratio_thres=0.15),
+                     RandomCropWithPadding((IMAGE_H,IMAGE_W),IGNORE_LABEL),
+                     imgaug.Flip(horiz=True),
+                     ]
+    else:
+        shape_aug = []
+
+    ds = AugmentImageComponents(ds, shape_aug, (0, 1), copy=False)
+
+    def f(ds):
+        return ds
+    if isTrain:
+        ds = MapData(ds,f)
+        ds = BatchData(ds, batch_size)
+
+        ds = PrefetchDataZMQ(ds, 1)
+    else:
+        ds = BatchData(ds, 1)
+    return ds
+
+
+def view_data( meta_dir, batch_size):
+    ds = RepeatedData(get_data('train',meta_dir, batch_size), -1)
     ds.reset_state()
     for ims, labels in ds.get_data():
         for im, label in zip(ims, labels):
@@ -148,12 +152,13 @@ def view_data(data_dir, meta_dir, batch_size):
             cv2.waitKey(0)
 
 
-def get_config(data_dir, meta_dir, batch_size):
+def get_config(meta_dir, batch_size):
     logger.auto_set_dir()
     nr_tower = max(get_nr_gpu(), 1)
-    dataset_train = get_data('train_aug', data_dir, meta_dir, batch_size)
+
+    dataset_train = get_data('train', meta_dir, batch_size)
     steps_per_epoch = dataset_train.size() * epoch_scale
-    dataset_val = get_data('val', data_dir, meta_dir, batch_size)
+    dataset_val = get_data('val',  meta_dir, batch_size)
 
     return TrainConfig(
         dataflow=dataset_train,
@@ -166,7 +171,7 @@ def get_config(data_dir, meta_dir, batch_size):
         ],
         model=Model(),
         steps_per_epoch=steps_per_epoch,
-        max_epoch=max_epoch,
+        max_epoch=10,
         nr_tower = nr_tower
     )
 
@@ -195,7 +200,8 @@ def run(model_path, image_path, output):
 
 def proceed_validation(args, is_save = False, is_densecrf = False):
     import cv2
-    ds = dataset.PascalVOC12(args.data_dir, args.meta_dir, "val")
+    name = "val"
+    ds = dataset.Cityscapes(args.meta_dir, name)
     ds = BatchData(ds, 1)
 
     pred_config = PredictConfig(
@@ -204,6 +210,10 @@ def proceed_validation(args, is_save = False, is_densecrf = False):
         input_names=['image'],
         output_names=['prob'])
     predictor = OfflinePredictor(pred_config)
+
+    from tensorpack.utils.fs import mkdir_p
+    result_dir = "validation-{}".format(os.path.basename(__file__).rstrip(".py"))
+    mkdir_p(result_dir)
 
     i = 0
     stat = MIoUStatistics(CLASS_NUM)
@@ -214,16 +224,15 @@ def proceed_validation(args, is_save = False, is_densecrf = False):
         # output : H*W*C
         output = predictor(input_img)
         return output[0][0]
-
     for image, label in tqdm(ds.get_data()):
         label = np.squeeze(label)
         image = np.squeeze(image)
-        prediction = predict_scaler(image, mypredictor, scales=[0.5,0.75, 1, 1.25, 1.5], classes=CLASS_NUM, tile_size=CROP_SIZE, is_densecrf = is_densecrf)
+        prediction = predict_scaler(image, mypredictor, scales=[0.5,0.75, 1, 1.25, 1.5], classes=CLASS_NUM, tile_size=(IMAGE_H,IMAGE_W), is_densecrf = is_densecrf)
         prediction = np.argmax(prediction, axis=2)
         stat.feed(prediction, label)
 
         if is_save:
-            cv2.imwrite("result/{}.png".format(i), np.concatenate((image, visualize_label(label), visualize_label(prediction)), axis=1))
+            cv2.imwrite(os.path.join(result_dir,"{}.png".format(i)), np.concatenate((image, visualize_label(label), visualize_label(prediction)), axis=1))
 
         i += 1
 
@@ -248,7 +257,7 @@ class CalculateMIoU(Callback):
 
     def _trigger(self):
         global args
-        self.val_ds = get_data('val', args.data_dir, args.meta_dir, args.batch_size)
+        self.val_ds = get_data('val', args.meta_dir, args.batch_size)
         self.val_ds.reset_state()
 
         self.stat = MIoUStatistics(self.nb_class)
@@ -262,7 +271,7 @@ class CalculateMIoU(Callback):
         for image, label in tqdm(self.val_ds.get_data()):
             label = np.squeeze(label)
             image = np.squeeze(image)
-            prediction = predict_scaler(image, mypredictor, scales=[0.5,0.75, 1, 1.25, 1.5], classes=CLASS_NUM, tile_size=CROP_SIZE,
+            prediction = predict_scaler(image, mypredictor, scales=[0.5,0.75, 1, 1.25, 1.5], classes=CLASS_NUM, tile_size=(IMAGE_H,IMAGE_W),
                            is_densecrf=False)
             prediction = np.argmax(prediction, axis=2)
             self.stat.feed(prediction, label)
@@ -271,15 +280,12 @@ class CalculateMIoU(Callback):
         self.trainer.monitors.put_scalar("mean_accuracy", self.stat.mean_accuracy)
         self.trainer.monitors.put_scalar("accuracy", self.stat.accuracy)
 
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', default="0", help='comma separated list of GPU(s) to use.')
-    parser.add_argument('--data_dir', default="/data2/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012",
-                        help='dataset dir')
-    parser.add_argument('--meta_dir', default="../metadata/pascalvoc12", help='meta dir')
+    parser.add_argument('--meta_dir', default="../metadata/cityscapes", help='meta dir')
     parser.add_argument('--load', default="../resnet101.npz", help='load model')
+    #parser.add_argument('--load', default="train_log/deeplabv2.naked.cs/model-26712", help='load model')
     parser.add_argument('--view', help='view dataset', action='store_true')
     parser.add_argument('--run', help='run model on images')
     parser.add_argument('--batch_size', type=int, default = batch_size, help='batch_size')
@@ -291,13 +297,13 @@ if __name__ == '__main__':
 
 
     if args.view:
-        view_data(args.data_dir,args.meta_dir,args.batch_size)
+        view_data(args.meta_dir,args.batch_size)
     elif args.run:
         run(args.load, args.run, args.output)
     elif args.validation:
         proceed_validation(args)
     else:
-        config = get_config(args.data_dir,args.meta_dir,args.batch_size)
+        config = get_config(args.meta_dir,args.batch_size)
         if args.load:
             config.session_init = get_model_loader(args.load)
         launch_train_with_config(
