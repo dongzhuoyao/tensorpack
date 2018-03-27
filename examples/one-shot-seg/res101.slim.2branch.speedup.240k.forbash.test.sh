52effc4dfc61c572f5e5570ad6ebb2916b7756aa
Tao Hu <taohu620@gmail.com>
2018-03-27 22:22:54 +0800

    u

--- examples/one-shot-seg/res101.slim.2branch.speedup.240k.forbash.test.sh
+++ examples/one-shot-seg/res101.slim.2branch.speedup.240k.forbash.test.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 
-python res101.slim.2branch.speedup.240k.forbash.py --gpu 3\
-  --train_data fold0_train\
+python res101.slim.2branch.speedup.240k.forbash.py --gpu 4\
+  --test\
   --test_data fold0_1shot_test\
   --test_load train_log/res101.slim.2branch.speedup.240k.forbash:fold0_1shot_test/model-5582628 >> result_test_txt
 
-python res101.slim.2branch.speedup.240k.forbash.py --gpu 3\
-  --train_data fold1_train\
+python res101.slim.2branch.speedup.240k.forbash.py --gpu 4\
+  --test\
   --test_data fold1_1shot_test\
    --test_load train_log/res101.slim.2branch.speedup.240k.forbash:fold1_1shot_test/model-5582628 >> result_test_txt
 
-python res101.slim.2branch.speedup.240k.forbash.py --gpu 3\
-  --train_data fold2_train\
+python res101.slim.2branch.speedup.240k.forbash.py --gpu 4\
+  --test\
   --test_data fold2_1shot_test\
    --test_load train_log/res101.slim.2branch.speedup.240k.forbash:fold2_1shot_test/model-5579295 >> result_test_txt
 
-python res101.slim.2branch.speedup.240k.forbash.py --gpu 3\
-  --train_data fold3_train\
+python res101.slim.2branch.speedup.240k.forbash.py --gpu 4\
+  --test\
   --test_data fold3_1shot_test\
    --test_load train_log/res101.slim.2branch.speedup.240k.forbash:fold3_1shot_test/model-5582628 >> result_test_txt
