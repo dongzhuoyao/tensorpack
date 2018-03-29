001e2f6fabe30e0f701477b5ecb4313b4af2dcac
Tao Hu <taohu620@gmail.com>
2018-03-30 00:19:43 +0800

    u

--- examples/one-shot-seg/res101.slim.2branch.speedup.mcontext.240k.support_se.batch.test5shot.sh
+++ examples/one-shot-seg/res101.slim.2branch.speedup.mcontext.240k.support_se.batch.test5shot.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold0_train\
+python res101.slim.2branch.speedup.mcontext.240k.support_se.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.support_se:fold0_1shot_test/model-557929\
-  --test_data fold0_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold0_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.support_se.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold1_train\
+python res101.slim.2branch.speedup.mcontext.240k.support_se.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.support_se:fold1_1shot_test/model-5579295\
-  --test_data fold1_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold1_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.support_se.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold2_train\
+python res101.slim.2branch.speedup.mcontext.240k.support_se.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.support_se:fold2_1shot_test\
-  --test_data fold2_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold2_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.support_se.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold3_train\
+python res101.slim.2branch.speedup.mcontext.240k.support_se.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.support_se:fold3_1shot_test/model-5582628\
-  --test_data fold3_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold3_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.support_se.5shot.result
--- examples/one-shot-seg/res101.slim.2branch.speedup.mcontext.240k.width512.batch.test5shot.sh
+++ examples/one-shot-seg/res101.slim.2branch.speedup.mcontext.240k.width512.batch.test5shot.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold0_train\
+python res101.slim.2branch.speedup.mcontext.240k.width512.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.width512:fold0_1shot_test/model-5582628\
-  --test_data fold0_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold0_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.width512.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold1_train\
+python res101.slim.2branch.speedup.mcontext.240k.width512.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.width512:fold1_1shot_test/model-5579295\
-  --test_data fold1_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold1_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.width512.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold2_train\
+python res101.slim.2branch.speedup.mcontext.240k.width512.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.width512:fold2_1shot_test/model-5575962\
-  --test_data fold2_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold2_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.width512.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
-  --train_data fold3_train\
+python res101.slim.2branch.speedup.mcontext.240k.width512.py --gpu 3\
+  --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.width512:fold3_1shot_test/model-5582628\
-  --test_data fold3_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.batch.result
+  --test_data fold3_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.width512.5shot.result
