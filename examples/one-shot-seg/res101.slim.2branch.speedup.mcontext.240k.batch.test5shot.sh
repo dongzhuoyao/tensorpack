e76108d28f10f4ce13c47f0ba7437fc564f669c8
Tao Hu <taohu620@gmail.com>
2018-03-30 09:21:01 +0800

    u

--- examples/one-shot-seg/res101.slim.2branch.speedup.mcontext.240k.batch.test5shot.sh
+++ examples/one-shot-seg/res101.slim.2branch.speedup.mcontext.240k.batch.test5shot.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
+python res101.slim.2branch.speedup.mcontext.240k.py --gpu 5\
   --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k:fold0_1shot_test/model-5582628\
-  --test_data fold0_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
+  --test_data fold0_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
+python res101.slim.2branch.speedup.mcontext.240k.py --gpu 5\
   --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k:fold1_1shot_test/model-5579295\
-  --test_data fold1_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
+  --test_data fold1_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
+python res101.slim.2branch.speedup.mcontext.240k.py --gpu 5\
   --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k:fold2_1shot_test/model-5582628\
-  --test_data fold2_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
+  --test_data fold2_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
 
-python res101.slim.2branch.speedup.mcontext.240k.py --gpu 3\
+python res101.slim.2branch.speedup.mcontext.240k.py --gpu 5\
   --test\
   --test_load train_log/res101.slim.2branch.speedup.mcontext.240k:fold3_1shot_test/model-5582628\
-  --test_data fold3_1shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
+  --test_data fold3_5shot_test >> res101.slim.2branch.speedup.mcontext.240k.5shot.result
