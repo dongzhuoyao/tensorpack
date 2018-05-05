2a0637019dbb0c1a66237347b324490c7bf6fefe
Tao Hu <taohu620@gmail.com>
2018-05-05 15:03:20 +0800

    u

--- tensorpack/gist/kl_loss.py
+++ tensorpack/gist/kl_loss.py
@@ -1,11 +1,11 @@
 # Author: Tao Hu <taohu620@gmail.com>
 import tensorflow as tf
 
-def kl_loss_compute(logits1, logits2):
+def kl_loss_compute(logits1, logits2, name):
     """ KL loss
     """
     pred1 = tf.nn.softmax(logits1)
     pred2 = tf.nn.softmax(logits2)
-    loss = tf.reduce_mean(tf.reduce_sum(pred2 * tf.log(1e-8 + pred2 / (pred1 + 1e-8)), 1))
+    loss = tf.reduce_mean(tf.reduce_sum(pred2 * tf.log(1e-8 + pred2 / (pred1 + 1e-8)), 1),name=name)
 
     return loss
