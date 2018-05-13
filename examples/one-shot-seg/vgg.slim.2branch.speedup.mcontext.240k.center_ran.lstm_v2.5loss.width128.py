b7a6604b9f8224953601f81ed397777a97bd539f
Tao Hu <taohu620@gmail.com>
2018-05-13 14:34:54 +0800

    u

--- examples/one-shot-seg/vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py
+++ examples/one-shot-seg/vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py
@@ -35,23 +35,24 @@ batch_size = 4
 k_shot = 5
 
 from cell import ConvLSTMCell_carlthome
+"""
+python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-60000 --k_shot 1 --test --test_data fold0_1shot_test --gpu 5 &&
+python  vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load   train_log/vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold1_5shot_test/model-60000 --k_shot 1 --test --test_data fold1_1shot_test --gpu 5 &&
+python  vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load   train_log/vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold2_5shot_test/model-60000 --k_shot 1 --test --test_data fold2_1shot_test --gpu 5 &&
+python  vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load   train_log/vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold3_5shot_test/model-50000 --k_shot 1 --test --test_data fold3_1shot_test --gpu 5
 
-#python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 1 --test --test_data fold0_1shot_test --gpu 2
-#python  res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load   train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold1_5shot_test/model-48000 --k_shot 1 --test --test_data fold1_1shot_test --gpu 1
-#python  res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load   train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold2_5shot_test/model-48000 --k_shot 1 --test --test_data fold2_1shot_test --gpu 1
-#python  res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load   train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold3_5shot_test/model-32000 --k_shot 1 --test --test_data fold3_1shot_test --gpu 1
+"""
 
 
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 2 --test --test_data fold0_2shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 3 --test --test_data fold0_3shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 4 --test --test_data fold0_4shot_test --gpu 2
 
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 2 --test --test_data fold0_2shot_test --gpu 2
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 3 --test --test_data fold0_3shot_test --gpu 2
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 4 --test --test_data fold0_4shot_test --gpu 2
-
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 6 --test --test_data fold0_6shot_test --gpu 2
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 7 --test --test_data fold0_7shot_test --gpu 2
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 8 --test --test_data fold0_8shot_test --gpu 2
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 9 --test --test_data fold0_9shot_test --gpu 2
-#  python res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 10 --test --test_data fold0_10shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 6 --test --test_data fold0_6shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 7 --test --test_data fold0_7shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 8 --test --test_data fold0_8shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 9 --test --test_data fold0_9shot_test --gpu 2
+#  python vgg.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128.py --test_load train_log/res101.slim.2branch.speedup.mcontext.240k.center_ran.lstm_v2.5loss.width128:fold0_5shot_test/model-48000 --k_shot 10 --test --test_data fold0_10shot_test --gpu 2
 
 def my_squeeze_excitation_layer(input_x, out_dim, layer_name,ratio=4):
   with tf.variable_scope(layer_name):
@@ -380,7 +381,7 @@ def proceed_test(args, is_save = False):
         def mypredictor(input_img):
             # input image: 1*H*W*3
             # output : H*W*C
-            output = predictor(first_image_masks[np.newaxis, :, :, :, :], input_img)
+            output = predictor(first_image_masks[np.newaxis, :, :, :, :], input_img[np.newaxis,:, :, :])
             return output[0][0]
 
         prediction = predict_scaler(second_image, mypredictor, scales=[0.5, 0.75, 1, 1.25, 1.5],
